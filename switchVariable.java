78bc535c5c24c0d2ba92c52713fce6d8d53adffe
Saniverse19 <166517153+Saniverse19@users.noreply.github.com>
2024-04-09 18:41:46 +0530

    Create (switchVariable.java)

---
+++ switchVariable.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String x ="water";
+		String y ="kool-Aid";
+		
+		String temp=null;
+		temp =x;
+		x=y;
+		y=temp;
+		
+		System.out.println("x :"+x);
+		System.out.println("y :"+y);
+		
+		
+
+	}
+
+}
